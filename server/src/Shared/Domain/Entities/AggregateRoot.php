90e52429b83fd86b1d718abede9af4295bdef000
laubernal <lbernalrodrguez@gmail.com>
2023-12-29 12:35:50 +0100

    implement aggregate root

--- server/src/Shared/Domain/Entities/AggregateRoot.php
+++ server/src/Shared/Domain/Entities/AggregateRoot.php
@@ -7,7 +7,25 @@ use IdVo;
 
 class AggregateRoot
 {
-    public function __construct(IdVo $id, DateTime $createdAt, DateTime $updatedAt)
+    protected function __construct(
+        private IdVo $id,
+        private DateTime $createdAt = new DateTime(),
+        private DateTime $updatedAt = new DateTime()
+    ) {
+    }
+
+    public function id(): IdVo
     {
+        return $this->id;
+    }
+
+    public function createdAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function updatedAt(): DateTime
+    {
+        return $this->updatedAt;
     }
 }
